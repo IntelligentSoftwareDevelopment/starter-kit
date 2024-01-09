44dce606c7e505b336f3b057dddebf44eeedd86f
Ivan Rusev <ins0mniac1van@gmail.com>
2024-01-09 12:21:20 +0200

    Initial commit

---
+++ src/StarterKitServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+namespace IntelligentSoftwareDevelopment\StarterKit;
+
+use Illuminate\Support\ServiceProvider;
+
+class StarterKitServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+
+    }
+
+    public function boot(): void
+    {
+
+    }
+}
